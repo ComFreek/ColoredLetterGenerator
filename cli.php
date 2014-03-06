ffb3960082e10673901cfbc8ac32e7a8d90ccebd
ComFreek <comfreek@outlook.com>
2014-03-06 14:09:59 +0100

    Initial commit

---
+++ cli.php
@@ -0,0 +1,94 @@
+<?php
+
+// Colored Letter Generator
+namespace CLG;
+
+require_once('generator.php');
+
+/*
+ * Format of config file:
+ * JSON
+ * {
+ * 		"width": 100,
+ *  	"height": 100,
+ *  	"font": "xyz.ttf",
+ *  	"letters": [
+ *  		"a","b","c",...
+ *  	],
+ *  	"colors": [
+ *  		[255,0,0],
+ *   		[0,255,0],
+ *  		...
+ *   	]
+ * }
+ * NOTE: this function also normalized the font path
+ */
+function readConfigFile($file) {
+	$contents = @file_get_contents($file);
+	if ($contents === false) {
+		return false;
+	}
+	$json = json_decode($contents, true);
+	if ($json === null) {
+		return false;
+	}
+	
+	// normalize font path
+	// if the font path is NOT absolute (i.e. relative)
+	if (!preg_match('/^(?:\/|\\\\|\w:\\\\|\w:\/).*$/', $json['font'])) {
+		$json['font'] = realpath(dirname(realpath($file)) . DIRECTORY_SEPARATOR . $json['font']);
+	}
+	
+	return $json;
+}
+
+function genImages($options, $saveCallback) {
+	$gen = new Generator(['width' => $options['width'], 'height' => $options['height']], $options['font']);
+	$letters = $options['letters'];
+	
+	$nrOfLetters = count($options['letters']);
+	$nrOfColors = count($options['colors']);
+	for ($letterIdx = 0; $letterIdx < $nrOfLetters; $letterIdx++) {
+		$letter = $options['letters'][$letterIdx];
+	
+		for ($colorIdx = 0; $colorIdx < $nrOfColors; $colorIdx++) {
+			$color = $options['colors'][$colorIdx];
+		
+			$letterImg = $gen->makeLetter(
+				$letter,
+				['r' => $color[0], 'g' => $color[1], 'b' => $color[2]]
+			);
+			
+			$saveCallback($letterImg, $letter, $color, ['r' => 255, 'g' => 255, 'b' => 255]);
+			imagedestroy($letterImg);
+		}
+	}
+}
+
+function saveInFolder($path) {
+	return function($img, $letter, $color, $bgColor) use ($path) {
+		$bgPath = $path . DIRECTORY_SEPARATOR . implode('-', $bgColor);
+		if (!file_exists($bgPath)) {
+			mkdir($bgPath);
+		}
+		$filename = $letter . '_' . implode('-', $color) . '.png';
+		imagepng($img, $bgPath . DIRECTORY_SEPARATOR . $filename);
+	};
+}
+
+echo 'Path to your configuration file: ';
+$configFile = trim(fgets(STDIN));
+
+$options = readConfigFile($configFile);
+if ($options === false) {
+	exit('Could not read configuration file. Check file path and/or JSON syntax.');
+}
+
+echo 'Path to destination folder: ';
+$destFolder = trim(fgets(STDIN));
+
+if (!file_exists($destFolder)) {
+	mkdir($destFolder);
+}
+genImages($options, saveInFolder($destFolder));
+
---
+++ generator.php
@@ -0,0 +1,100 @@
+<?php
+
+// Colored Letter Generator
+namespace CLG;
+
+require_once('calculateTextBox.php');
+
+/**
+ * Returns the dimension a specific text needs
+ * Note that this function does not take letter-specific whitespace on the left
+ * and right sides into account!
+ * We therefore don't use this function for centering the letters
+ * @see calculateTextBox()
+ * 
+ * @param integer $size Font size
+ * @param float $angle Angle
+ * @param string $text Text
+ * @return The dimension saved as ['width' => width, 'height' => height]
+ */
+function getDimensionOfText($size, $angle, $font, $text) {
+	$bbox = imagettfbbox($size, $angle, $font, $text);
+	$width = max($bbox[2], $bbox[4]) - max($bbox[0], $bbox[6]);
+	$height = max($bbox[1], $bbox[3]) - max($bbox[5], $bbox[7]);
+	
+	return compact('width', 'height');
+}
+
+class Generator {
+	private $letterSize;
+	private $font;
+	
+	/**
+	 * @param array $letterSize Size of the graphics: ['width' => ..., 'height' => ...]
+	 * @param string $font Path to the font file (may be relative or absolute)
+	 */
+	public function __construct(Array $letterSize, $font) {
+		$this->letterSize = $letterSize;
+		$this->font = $font;
+	}
+
+	/**
+	 * Calculates the maximum font size for $letter which would still
+	 * fit into the bounding box (=$letterSize values)
+	 * @param string $letter
+	 * @return integer Font size
+	 */
+	private function calcMaxSize($letter) {
+		$lastGoodSize = null;
+		for ($size=1; $size<500; $size++) {
+			$sizeData = imagettfbbox($size, 0, $this->font, $letter);
+			$dimension = getDimensionOfText($size, 0, $this->font, $letter);
+
+			if ($dimension['width'] > $this->letterSize['width'] ||
+			    $dimension['height'] > $this->letterSize['height']) {
+				return $lastGoodSize;
+			}
+			$lastGoodSize = $size;
+		}
+		return $lastGoodSize;
+	}
+	
+	/**
+	 * Calculates the centered coordinates for use with imagettfbox()
+	 * @param string $letter
+	 * @param integer $size The letter's size
+	 * @return array ['x' => centeredXCoordinate, 'y' => centeredYCoordinate]
+	 */
+	private function getCenteredCoords($letter, $size) {
+		$bbox = calculateTextBox($size, 0, $this->font, $letter);
+		$centeredX = (($this->letterSize['width'] - $bbox['width']) / 2);
+		$centeredY = (($this->letterSize['height'] - $bbox['height']) / 2);
+		
+		return [
+			'x' => $centeredX + $bbox['left'],
+			'y' => $centeredY + $bbox['top']
+		];
+	}
+	
+	/*
+	 * @param string $letter Letter
+	 * @param Array $color Color, format: ['r' => 123, 'g' => 456, 'b' => 789]
+	 * @param Array $bgColor Background color, same format as $color
+	 * @return GD2 image resource (note: it's up to you to imagedestroy() it!)
+	 */
+	public function &makeLetter($letter, Array $color, $bgColor = ['r' => 255, 'g' => 255, 'b' => 255]) {
+		$img = imagecreatetruecolor($this->letterSize['width'], $this->letterSize['height']);
+		
+		$colorResource = imagecolorallocate($img, $color['r'], $color['g'], $color['b']);
+		$bgColorResource = imagecolorallocate($img, $bgColor['r'], $bgColor['g'], $bgColor['b']);
+		
+		$maxSize = $this->calcMaxSize($letter);
+		
+		$coords = $this->getCenteredCoords($letter, $maxSize);
+		
+		imagefill($img, 0, 0, $bgColorResource);
+		imagettftext($img, $maxSize, 0, $coords['x'], $coords['y'], $colorResource, $this->font, $letter);
+		
+		return $img;
+	}
+}
